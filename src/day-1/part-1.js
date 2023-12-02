d8b67d198ceb885126194fea8e98d2ff79ba4b82
Alvaro <alvaromagu2002@gmail.com>
2023-12-02 14:14:31 +0100

    initial commit

---
+++ src/day-1/part-1.js
@@ -0,0 +1,15 @@
+import {readFile} from 'node:fs/promises'
+
+const path = new URL('input.txt', import.meta.url)
+const input = await readFile(path).then(res => res.toString())
+const lines = input.split('\r\n')
+const numRegex = /[0-9]/g
+
+const linesNums = lines.map(line => {
+  const matches = line.match(numRegex)
+  return Number(`${matches[0]}${matches[matches.length - 1]}`)
+})
+
+const sum = linesNums.reduce((acc, cur) => acc + cur, 0)
+
+console.log(sum)
---
+++ src/day-1/part-2.js
@@ -0,0 +1,41 @@
+import {readFile} from 'node:fs/promises'
+
+const path = new URL('input.txt', import.meta.url)
+const input = await readFile(path).then(res => res.toString())
+const lines = input.split('\r\n')
+const numRegex = /one|two|three|four|five|six|seven|eight|nine|[0-9]/g
+
+const strToNum = {
+  'one': 1,
+  'two': 2,
+  'three': 3,
+  'four': 4,
+  'five': 5,
+  'six': 6,
+  'seven': 7,
+  'eight': 8,
+  'nine': 9
+}
+
+function matchOverlap (input, regex) {
+  const result = []
+  let match
+  while (match = regex.exec(input)) {
+    regex.lastIndex -= match[0].length - 1
+    result.push(match[0])
+  }
+  return result
+}
+
+const linesNums = lines.map(line => {
+  const matches = matchOverlap(line, numRegex)
+  const firstMatch = matches[0]
+  const firstNum = strToNum[firstMatch] ?? Number(firstMatch)
+  const lastMatch = matches[matches.length - 1]
+  const lastNum = strToNum[lastMatch] ?? Number(lastMatch)
+  return Number(`${firstNum}${lastNum}`)
+})
+
+const sum = linesNums.reduce((acc, cur) => acc + cur, 0)
+
+console.log(sum)
